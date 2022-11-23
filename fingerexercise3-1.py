c1731b8de67e5f5e2e4e770a46b127782de5f353
Ashley Speight <ashleyspeight86@gmail.com>
2022-11-23 22:48:12 +0100

    chapter 3 exercise

---
+++ fingerexercise3-1.py
@@ -0,0 +1,13 @@
+x = int(input("Enter an int "))
+
+ans = 0
+
+while ans ** 3 < abs(x):
+    ans = ans + 1
+if ans ** 3 != abs(x):
+    print(x, "is not a perfect cube")
+else:
+    if x < 0:
+        ans = - ans
+    print("cube root of", x, "is", ans)
+
