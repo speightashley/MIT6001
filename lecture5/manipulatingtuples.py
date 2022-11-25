5a96fe00859b547921613c04e57083e3590201db
Ashley Speight <ashleyspeight86@gmail.com>
2022-11-25 19:00:43 +0100

    problem set 2

---
+++ lecture5/manipulatingtuples.py
@@ -0,0 +1,12 @@
+def get_data(aTuple):
+    nums = ()
+    words = ()
+    for t in aTuple:
+        nums = nums + (t[0],)
+        if t[1] not in words:
+            words = words + (t[1],)
+    min_n = min(nums)
+    max_n = max(nums)
+    unique_words = len(words)
+    return (min_n, max_n, unique_words)
+
